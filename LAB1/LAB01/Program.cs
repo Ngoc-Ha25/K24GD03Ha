beec5fa19a1b4d422c2993c0d03a6ee7d355e976
Ngoc-Ha25 <152829720+Ngoc-Ha25@users.noreply.github.com>
2025-06-10 11:19:30 +0700

    Change program

---
+++ LAB1/LAB01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("Nhap so a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so b: ");
+            b = double.Parse(Console.ReadLine());
+            double Thuong = a / b;
+            double Tong = a + b;
+            double Hieu = a - b;
+            double Tich = a * b;
+            Console.WriteLine("Thuong la: " + Thuong);
+            Console.WriteLine("Tong la: " + Tong);
+            Console.WriteLine("Hieu la: " + Hieu);
+            Console.WriteLine("Tich la: " + Tich);
+            Console.ReadLine();
+
+        }
+    }
+}
