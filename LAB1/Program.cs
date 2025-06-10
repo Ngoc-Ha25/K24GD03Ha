0293b8c15f70f3c5647d931943283a5acfdf74ea
Ngoc-Ha25 <152829720+Ngoc-Ha25@users.noreply.github.com>
2025-06-10 10:43:26 +0700

    Update Program.cs

--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -4,8 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.Readline();
+            double a, b;
+            Console.WriteLine("Nhap so a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so b: ");
+            b = double.Parse(Console.ReadLine());
+            double Thuong = a / b;
+            double Tong = a + b;
+            double Hieu = a - b;
+            double Tich = a * b;
+            Console.WriteLine("Thuong la: ", Thuong);
+            Console.WriteLine("Tong la: ", Tong);
+            Console.WriteLine("Hieu la: ", Hieu);
+            Console.WriteLine("Tich la: ", Tich);
         }
     }
 }
