ecb1eed09c5f160f315043b0a1fe4e9245d59dc8
Ngoc-Ha25 <152829720+Ngoc-Ha25@users.noreply.github.com>
2025-09-13 05:17:11 +0700

    FINAL LAB

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using TMPro;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private float gameSpeed = 5f;
+    [SerializeField]
+    private float speedIncrease = 0.15f;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private float score = 0;
+    [SerializeField] private GameObject scoreTextObject;
+    [SerializeField] private GameObject gameStartMess;
+    [SerializeField] private GameObject gameOverMess;
+    private bool isGameOver = false;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public float GetGameSpeed()
+    {
+        return gameSpeed;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGameSpeed();
+        HandleStartGameInput();
+        
+        if(!isGameOver)
+        {
+            UpdateGameSpeed();
+            UpdateScore();
+        }
+    }
+    private void UpdateGameSpeed()
+    {
+        gameSpeed += Time.deltaTime + speedIncrease;
+    }
+    private void UpdateScore()
+    {
+        score += Time.deltaTime * 10;
+        scoreText.text= ("Score:") + Mathf.FloorToInt(score);
+    }
+    private void StartGame()
+    {
+        Time.timeScale = 0;
+        scoreTextObject.SetActive(false);
+        gameStartMess.SetActive(true);
+        gameOverMess.SetActive(false);
+    }
+    private void HandleStartGameInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Time.timeScale = 1;
+            scoreTextObject.SetActive(true);
+            gameStartMess.SetActive(false);
+        }
+    }
+    public void GameOver()
+    {
+        isGameOver = true;
+        gameOverMess.SetActive(true);
+        Time.timeScale = 0;
+        StartCoroutine(ReloadScene());
+    }
+    private IEnumerator ReloadScene()
+    {
+        yield return new WaitForSecondsRealtime(2f); // dùng thời gian thực
+        Time.timeScale = 1; // khôi phục thời gian trước khi load lại
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
---
+++ Scripts/spawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstacles;
+    [SerializeField] private Transform highPos;
+    [SerializeField] private Transform lowPos;
+    private float timer = 0;
+    [SerializeField] private float spawnRate = 2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > spawnRate)
+        {
+            SpawnObstacle();
+            timer = 0;
+        }
+    }
+    private void SpawnObstacle()
+    {
+        int index = Random.Range(0, obstacles.Length);
+        if (index == 0 || index == 1)
+        {
+            GameObject obstacle = Instantiate(obstacles[index], lowPos.position, Quaternion.identity);
+        }
+        else if(index == 2)
+        {
+            GameObject obstacle = Instantiate(obstacles[index], highPos.position, Quaternion.identity);
+        }
+    }
+}
